a6e32221e861c9b82c30a28345b7c5817b84f722
F-Saint <patterco27@gmail.com>
2024-01-30 15:04:01 +0100

    mon premier commit

---
+++ PHP_SQY_ESHOP/partials/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eshop PHP</title>
+    <link rel="stylesheet" href="..\style.css">
+
+</head>
+<body>
+
+<nav>
+    <ul>
+        <li><a href="..\views\home.view.php">Accueil</a></li>
+        <li><a href="..\views\products.view.php">Produits</a></li>
+        <li><a href="..\views\contact.view.php">Contact</a></li>
+        <li><a href="signup.php">Signup</a></li>
+    </ul>
+</nav>
---
+++ PHP_SQY_ESHOP/views/home.view.php
@@ -0,0 +1,8 @@
+<?php include '../partials/header.php'; ?>
+
+    <div class="wrapper">
+    <h1>Bienvenue sur mon shop PHP</h1>
+        <img class="logo" src="../assets\img\modern-company-logo-design-vector.jpg" alt="">
+    </div>
+    
+<?php include '../partials/footer.php'; ?>
